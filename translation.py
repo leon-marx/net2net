dfaa7bc2518e9840c0878fe429c5a3b49fa0a60e
leon-marx <gooogleon@gmail.com>
2021-11-01 17:12:12 +0100

    minor changes

--- translation.py
+++ translation.py
@@ -492,8 +492,12 @@ if __name__ == "__main__":
         data.setup()
 
         # configure learning rate
-        bs, base_lr, ngpu = config.data.params.batch_size, config.model.base_learning_rate, len(
-            lightning_config.trainer.gpus.strip(",").split(','))
+        try:
+            bs, base_lr, ngpu = config.data.params.batch_size, config.model.base_learning_rate, len(
+                lightning_config.trainer.gpus.strip(",").split(','))
+        except AttributeError:
+            bs, base_lr, ngpu = config.data.params.batch_size, config.model.base_learning_rate, len(
+                ["0"])
         accumulate_grad_batches = lightning_config.trainer.accumulate_grad_batches or 1
         model.learning_rate = accumulate_grad_batches * ngpu * bs * base_lr
         print("Setting learning rate to {:.2e} = {} (accumulate_grad_batches) * {} (num_gpus) * {} (batchsize) * {:.2e} (base_lr)".format(
@@ -513,8 +517,8 @@ if __name__ == "__main__":
                 import pudb; pudb.set_trace()
 
         import signal
-        signal.signal(signal.SIGUSR1, melk)
-        signal.signal(signal.SIGUSR2, divein)
+        # signal.signal(signal.SIGUSR1, melk)
+        # signal.signal(signal.SIGUSR2, divein)
 
         # run
         if opt.train:
