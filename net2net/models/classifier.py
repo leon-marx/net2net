77d6fbdeddd7db23c9d4e8c64ba9ad0709630f2e
leon-marx <gooogleon@gmail.com>
2021-12-08 15:05:24 +0100

    bug fix in classifier.py

--- net2net/models/classifier.py
+++ net2net/models/classifier.py
@@ -42,7 +42,6 @@ class Classifier(pl.LightningModule):
             print(f"Unexpected keys in state dict: {unexpected}")
 
     def forward(self, x):
-        oo = self.ae(torch.Tensor(x["image"]).view(1, 256, 256, 3).permute(0, 3, 1, 2))
         feats = self.ae.encoder(torch.Tensor(x["image"]).view(1, 256, 256, 3).permute(0, 3, 1, 2))
         z = self.ae.fc(feats)
         pred = self.linear(z)
